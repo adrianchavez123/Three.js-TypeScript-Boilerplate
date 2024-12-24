fb6859b156ccd841a92c6fbe846bd45efdec6649
Manuel Adrian Chavez Sanchez <manuel.adrian.chavez@ibm.com>
2024-12-24 08:07:37 -0600

    animation --rerender

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,55 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import Stats from "three/addons/libs/stats.module.js";
 import "./style.css";
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
   75,
-  2, //400/200
+  window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 camera.position.z = 1.5;
 
-const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true }); //smoother
-renderer.setSize(400, 200);
-//document.body.appendChild(renderer.domElement);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// window.addEventListener("resize", () => {
-//   camera.aspect = window.innerWidth / window.innerHeight;
-//   camera.updateProjectionMatrix();
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-// });
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+});
 
-new OrbitControls(camera, renderer.domElement);
-
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.addEventListener("change", function () {
+  renderer.render(scene, camera);
+});
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshNormalMaterial({ wireframe: true });
 
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+const stats = new Stats();
+document.body.appendChild(stats.dom);
+
+const clock = new THREE.Clock();
+let delta;
 function animate() {
   requestAnimationFrame(animate);
+  delta = clock.getDelta();
+  // cube.rotation.x += delta;
+  // cube.rotation.y += delta;
 
-  renderer.render(scene, camera);
+  // renderer.render(scene, camera);
+
+  stats.update();
 }
 
 animate();
+
+renderer.render(scene, camera);
