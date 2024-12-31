30c92c18ac39ae58629460dc38b27ca4aee5f3b2
Manuel Adrian Chavez Sanchez <manuel.adrian.chavez@ibm.com>
2024-12-31 10:51:55 -0600

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
   assetsInclude: ["**/*.hdr", "**/*.glb"],
+  plugins: [topLevelAwait()],
+  base: "./",
 });
